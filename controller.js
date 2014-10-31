a60e1dd6c14a4fd6f13b39deb562e6f8e7f234bc
amit giri <amit.giri@emids.com>
2014-10-31 09:22:31 +0530

    first commit

---
+++ controller.js
@@ -0,0 +1,20 @@
+﻿var app = angular.module('app', ['ui.bootstrap']);
+function controller($scope,$http) {
+    $scope.name = 'name';
+    $scope.open = function () {
+        $http({
+            method: 'GET',
+            url: 'http://vm-stage-01.cloudapp.net:3000/modules',
+            
+            header: {
+                'content-type' : 'application/json;charset=utf-8'
+            }
+        }).success(function (res) {
+            debugger;
+        }).error(function (res) {
+            debugger;
+        });
+
+        angular.element('.dud').show();
+    };
+};
