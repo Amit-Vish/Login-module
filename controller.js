1029568d99a06b9f4e48d9bda1f49e1b8df8670d
amit giri <amit.giri@emids.com>
2015-01-04 23:28:41 +0530

    login module

--- controller.js
+++ controller.js
@@ -1,13 +1,60 @@
-﻿var app = angular.module('app',[]);
-app.controller('crudController',['$scope','svc', function (scope,svc) {
+﻿(function (app) {
+  app.run(function ($rootScope) {
+    $rootScope.modalOption = {
+      templateUrl: 'myModalContent.html',
+      controller: 'LoginModalCtrl',
+      controllerAs: 'LoginModalCtrl',
+
+    };
+    $rootScope.defaultPage = 'login';
+  });
+  app.config(function ($stateProvider,$locationProvider) {
+    $locationProvider.html5Mode(true);
+    $locationProvider.hashPrefix = '!';
+    $stateProvider
+      .state('welcome', {
+        url: '/welcome',
+        templateUrl: "welcome.html",
+        data: {
+          requireLogin: true
+        }
+      })
+      .state('login', {
+        url: '/login',
+        templateUrl: "login.html",
+        // ...
+        data: {
+          requireLogin: false // this property will apply to all children of 'app'
+        }
+      })
+
+  });
+  app.controller('crudController', ['$scope', 'svc', function (scope, svc) {
     scope.name = 'amit';
     scope.addPopUp = function () {
-        angular.element('.addPopup').show();
+      angular.element('.addPopup').show();
     };
     scope.closePopup = function () {
-        angular.element('.addPopup').hide();
+      angular.element('.addPopup').hide();
     };
-    svc.getResult().then(function () {
-        debugger;
-    });
-}]);
+    this.login = function () {
+      svc.getResult().then(function () {
+      });
+    };
+    
+  }]);
+  app.controller('LoginModalCtrl', ['$scope', 'svc', '$modalInstance', '$rootScope', function (scope, svc, modalInstance, $rootScope) {
+    scope.name = 'hello';
+    scope.ok = function () {
+      modalInstance.close('something');
+    };
+
+    scope.cancel = function () {
+      modalInstance.dismiss('cancel');
+    };
+  }]);
+
+
+}(window.app))
+
+
---
+++ logmod.js
@@ -0,0 +1,64 @@
+﻿angular.module('logmod', [])
+  .service('loginModal', function ($modal, $rootScope,$q) {
+    function assignCurrentUser(user) {
+      $rootScope.currentUser = user;
+      return user;
+    }
+
+    return function (options) {
+      var instance = $modal.open(options);
+      return instance.result.then(assignCurrentUser);
+    };
+
+  })
+  .run(function ($rootScope, $state, loginModal) {
+    $rootScope.$on('$stateChangeStart', function (event, toState, toParams) {
+      if (toState.data.requireLogin && typeof $rootScope.currentUser === 'undefined') {
+        event.preventDefault();
+        loginModal($rootScope.modalOption)
+        .then(function (val) {
+          return $state.go(toState.name, toParams);
+        })
+        .catch(function (val) {
+          return $state.go($rootScope.defaultPage);
+        });
+      }
+    });
+  })
+  
+.config(function ($httpProvider, $locationProvider) {
+  $httpProvider.interceptors.push(function ($timeout, $q, $injector) {
+    var loginModal, $http, $state;
+
+    $timeout(function () {
+      loginModal = $injector.get('loginModal');
+      $http = $injector.get('$http');
+      $state = $injector.get('$state');
+    });
+
+    return {
+      'responseError': function (rejection) {
+        if (rejection.status !== 401) {
+          return rejection;
+        }
+
+        var deferred = $q.defer();
+
+        loginModal($rootScope.modalOption)
+          .then(function () {
+            deferred.resolve($http(rejection.config));
+          })
+          .catch(function () {
+            $state.go($rootScope.defaultPage);
+            deferred.reject(rejection);
+          });
+
+        return deferred.promise;
+      },
+      'request': function (config) {
+        return config;
+      }
+
+    };
+  });
+});
